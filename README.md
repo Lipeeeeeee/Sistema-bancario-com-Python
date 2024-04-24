65779151caec7e36648d46a692259ecfa96ad41d
Lipeeeeeee <favasconcelos09@gmail.com>
2024-04-24 13:56:27 -0300

    v2 do projeto

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Sistema bancário simples realizado como desafio de projeto no bootcamp Python AI backend da DIO patrocinado pela VIVO.
 
+# V1
+
 ## Funções propostas
 
 ### Depósito
@@ -15,3 +17,21 @@ Para sacar, existem mais condições, onde um saque também tem que ser positivo
 ### Extrato
 
 Somente mostra o extrato da conta após as operações de depósito e saque realizadas, no formato "OPERAÇÃO: R$ 0.00", e ao final mostrando o saldo no mesmo formato sendo OPERAÇÃO = saldo.
+
+# V2
+
+## Funções propostas
+
+### Modularização do código
+
+Foi proposto nesta versão para usarmos o conceito de modularização, a fim de organizarmos o código, criando funções para cada opção, fazendo com que somente sejam chamadas essas funções.
+
+Para a passagem de argumentos, a função Saque deveria ter todos os parâmetros recebidos por nome, a função Depositar deveria ter todos os parâmetros recebidos por posição, e a função Mostrar_extrato deveria ter parâmetros recebidos por posição e por nome
+
+### Criar usuário
+
+Os usuários criados deverão ser armazenados em uma lista. Cada usuário é composto por um nome, uma data de nascimento, um cpf, e um endereço, formado por logradouro, número, bairro e cidade acompanhada pela sigla do estado. Não pode cadastrar dois usuários com o mesmo CPF.
+
+### Criar conta corrente
+
+As contas criadas deverão ser armazenadas em uma lista. Cada conta é composta por uma agência, um número da conta, e um usuário, o dono da conta, sendo o número da conta um incremental e a agência uma constante "0001". O usuário pode ter mais de uma conta, mas uma conta pertence a um só usuário.
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,9 +1,81 @@
+def depositar(saldo, extrato, /):
+    deposito = int(input("\nInsira o valor que deseja depositar: "))
+    if deposito <= 0:
+        print("\nImpossível depositar valores negativos ou nenhum valor, por favor insira um valor positivo para depositar na próxima!")
+    else:
+        saldo += deposito
+        extrato += f"\nDEPÓSITO: R$ {deposito:.2f}"
+        print("\nDepósito efetuado com sucesso!")
+        return saldo, extrato
+
+def sacar(*, saldo, limite_saque, extrato):
+    if limite_saque == 0:
+        print("\nNão há mais saques disponíveis, tente de novo outro dia!")
+    else:
+        saque = int(input("\nInsira o valor que deseja sacar: "))
+        if saque <= 0:
+            print("\nImpossível sacar valores negativos ou nenhum valor, por favor insira um valor positivo para sacar na próxima!")
+        elif saque > 500:
+            print("\nValor do saque maior do que o limite, por favor insira um valor de 1 até 500!")
+        elif saque > saldo:
+            print(f"""
+Valor excede o valor do seu saldo em conta, que no momento é: R${saldo:.2f}
+Insira um valor de saque condizente com a quantidade de seu saldo""")
+        else:
+            saldo -= saque
+            limite_saque -= 1
+            extrato += f"\nSAQUE: R${saque:.2f}"
+            print("\nSaque efetuado com sucesso!")
+            return saldo, limite_saque, extrato
+        
+def mostrar_extrato(saldo, /, *, extrato):
+    print((extrato if extrato else "\nNão foram realizadas operações no momento.") + f"\n\nSALDO: R$ {saldo:.2f}")
+
+def sair(decisao_sair):
+    while decisao_sair != 1 and decisao_sair != 2:
+            print("\nTem certeza que deseja sair?")
+            decisao_sair = int(input("\nSelecione 1 para SIM ou 2 para NÃO: "))
+            if decisao_sair == 1:
+                print("\nEntão tá :( até outro dia!\n")
+                return 1
+            elif decisao_sair != 2:
+                print("\nSelecione uma das opções para sua decisão!")
+            else:
+                print("\nObrigado por continuar conosco :D")
+                return 0
+            
+def criar_usuario(usuarios):
+    cpf = input("Digite seu CPF (somente números): ")
+    for usuario in usuarios:
+        if cpf == usuario["cpf"]:
+            print("\nUsuário já existe em nosso banco!")
+            return usuarios
+    else:
+        nome = input("Digite seu nome: ")
+        data_nascimento = input("Digite sua data de nascimento (dd-mm-aaaa): ")
+        endereco = input("Digite seu endereço (logradouro, número - bairro - cidade/sigla_estado): ")
+        usuarios.append({"cpf": cpf, "nome": nome, "nascimento": data_nascimento, "endereço": endereco})
+        print(f"\nUsuário cadastrado com sucesso, seja bem-vindo {nome}!")
+        return usuarios
+
+def criar_conta(*, contas, usuarios):
+    cpf = input("Digite seu CPF (somente números): ")
+    for usuario in usuarios:
+        if cpf == usuario["cpf"]:
+            contas.append({"conta" : len(contas) + 1, "Agência" : "0001", "Conta_dono" : usuario["nome"]})
+            print("\nConta criada com sucesso!")
+            return contas
+    print("\nNão foi encontrado usuário com este CPF, insira um CPF existente entre nossos usuários!!")
+    return contas
+
+        
 saldo = 0
 limite_saque = 3
 extrato = """"""
 decisao_sair = 0
+usuarios = []
+contas = []
 while decisao_sair != 1:
-    decisao_sair = 0
     print("""
         Bem-vindo ao nosso banco!!
         O que deseja fazer hoje?
@@ -11,45 +83,22 @@ while decisao_sair != 1:
         1 - Depositar
         2 - Sacar
         3 - Ver extrato
-        4 - Sair
+        4 - Criar usuário
+        5 - Criar conta corrente
+        6 - Sair
     """)
     opcao = int(input())
     if opcao == 1:
-        deposito = int(input("\nInsira o valor que deseja depositar: "))
-        if deposito <= 0:
-            print("\nImpossível depositar valores negativos ou nenhum valor, por favor insira um valor positivo para depositar na próxima!")
-        else:
-            saldo += deposito
-            extrato += f"\nDEPÓSITO: R$ {deposito:.2f}"
-            print("\nDepósito efetuado com sucesso!")
+        saldo, extrato = depositar(saldo, extrato)
     elif opcao == 2:
-        if limite_saque == 0:
-            print("\nNão há mais saques disponíveis, tente de novo outro dia!")
-        else:
-            saque = int(input("\nInsira o valor que deseja sacar: "))
-            if saque <= 0:
-                print("\nImpossível sacar valores negativos ou nenhum valor, por favor insira um valor positivo para sacar na próxima!")
-            elif saque > 500:
-                print("\nValor do saque maior do que o limite, por favor insira um valor de 1 até 500!")
-            elif saque > saldo:
-                print(f"""
-Valor excede o valor do seu saldo em conta, que no momento é: R${saldo:.2f}
-Insira um valor de saque condizente com a quantidade de seu saldo""")
-            else:
-                saldo -= saque
-                limite_saque -= 1
-                extrato += f"\nSAQUE: R${saque:.2f}"
-                print("\nSaque efetuado com sucesso!")
+        saldo, limite_saque, extrato = sacar(saldo = saldo, limite_saque = limite_saque, extrato = extrato)
     elif opcao == 3:
-        print((extrato if extrato else "\nNão foram realizadas operações no momento.") + f"\n\nSALDO: R$ {saldo:.2f}")
+        mostrar_extrato(saldo, extrato = extrato)
     elif opcao == 4:
-        while decisao_sair != 1 and decisao_sair != 2:
-            print("\nTem certeza que deseja sair?")
-            decisao_sair = int(input("\nSelecione 1 para SIM ou 2 para NÃO: "))
-            if decisao_sair == 1:
-                print("\nEntão tá :( até outro dia!\n")
-                break
-            elif decisao_sair != 2:
-                print("\nSelecione uma das opções para sua decisão!")
-            else:
-                print("\nObrigado por continuar conosco :D")
+        usuarios = criar_usuario(usuarios)
+    elif opcao == 5:
+        contas = criar_conta(contas = contas, usuarios = usuarios)
+    elif opcao == 6:
+        decisao_sair = sair(decisao_sair)
+    else:
+        print("\nSelecione uma das opções do menu para prosseguir com sua ação!")
